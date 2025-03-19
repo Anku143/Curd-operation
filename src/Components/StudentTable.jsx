7986e0019332fefd89755320d8fd12c529de259b
Ankush <ankush.r5621@gmail.com>
2025-03-19 13:59:53 +0530

    Edit 2

--- src/Components/StudentTable.jsx
+++ src/Components/StudentTable.jsx
@@ -3,12 +3,34 @@ import { NavLink } from 'react-router-dom'
 const StudentTable = () => {
     return (
 
-        <div >
+        <div>
             <div className="container w-5/6 mx-auto">
                 <div className="main">
                     <h2 className=''>StudentTable</h2>
                     <div>
                         <NavLink to='/create' className='bg-[#ffa725] text-[#fff5e4] py-3 px-6 rounded-3xl transition-all duration-300 hover:bg-[#C1D8C3] hover:text-[#ffa725] inline-block'>Add New Student</NavLink>
+                    </div>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Course</th>
+                                    <th>Address</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
